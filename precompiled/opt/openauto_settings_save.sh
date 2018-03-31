8ba5693fed8911507db1b704a8f0e8c8095f2162
Huan Truong <htruong@tnhh.net>
2018-03-31 02:41:25 -0400

    [script] save and restore brightness settings

--- precompiled/opt/openauto_settings_save.sh
+++ precompiled/opt/openauto_settings_save.sh
@@ -2,6 +2,11 @@
 
 mount -o remount,rw /
 cp /tmp/openauto.ini ~/.openauto_saved.ini
+
+# try to save the brightness if possible
+if [ -f /sys/class/backlight/rpi_backlight/brightness ]; then
+	cat /sys/class/backlight/rpi_backlight/brightness > ~/.last_brightness
+fi
 sync
 mount -o remount,ro /
 

a768a2b427c1f29a5a6779de964a0093fc48c67b
Huan Truong <htruong@tnhh.net>
2018-03-31 03:07:22 -0400

    [script] add power management scripts

--- precompiled/opt/openauto_settings_save.sh
+++ precompiled/opt/openauto_settings_save.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mount -o remount,rw /
+
 cp /tmp/openauto.ini ~/.openauto_saved.ini
 
 # try to save the brightness if possible
@@ -8,5 +9,6 @@ if [ -f /sys/class/backlight/rpi_backlight/brightness ]; then
 	cat /sys/class/backlight/rpi_backlight/brightness > ~/.last_brightness
 fi
 sync
+
 mount -o remount,ro /
 
--- precompiled/opt/sleep.sh
+++ precompiled/opt/sleep.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+/opt/crankshaft/openauto_settings_save.sh
 echo 1 > /sys/class/backlight/rpi_backlight/bl_power
 vcgencmd display_power 0

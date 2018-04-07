049fa5d2ff4045e96716291c8896b610a647a4e9
Huan Truong <htruong@tnhh.net>
2018-04-06 22:40:42 -0400

    [script] update the update-udev script

--- update-udev-rules.sh
+++ update-udev-rules.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O precompiled/udev/51-android.rules https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
+wget -O crankshaft/rootfs/etc/udev/rules.d/51-android.rules https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules

a73f2a27191e5fe6c5490b92043f0d59fb0d739a
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-05-01 13:12:29 +0200

    [script] Fix needed libs on rpi_buildsystem

--- rpi_buildsystem/5_build_openauto.sh
+++ rpi_buildsystem/5_build_openauto.sh
@@ -15,6 +15,12 @@ rm -rf $HOME/openauto_build
 # Create build folder
 mkdir -p $HOME/openauto_build
 
+# link needed libs
+ln -s /opt/vc/lib/libbrcmEGL.so /usr/lib/arm-linux-gnueabihf/libEGL.so
+ln -s /opt/vc/lib/libbrcmGLESv2.so /usr/lib/arm-linux-gnueabihf/libGLESv2.so
+ln -s /opt/vc/lib/libbrcmOpenVG.so /usr/lib/arm-linux-gnueabihf/libOpenVG.so
+ln -s /opt/vc/lib/libbrcmWFC.so /usr/lib/arm-linux-gnueabihf/libWFC.so
+
 # Create inside build folder
 cd $HOME/openauto_build
 cmake -DCMAKE_BUILD_TYPE=Release -DRPI3_BUILD=TRUE -DAASDK_INCLUDE_DIRS="$HOME/aasdk/include" -DAASDK_LIBRARIES="$HOME/aasdk/lib/libaasdk.so" -DAASDK_PROTO_INCLUDE_DIRS="$HOME/aasdk_build" -DAASDK_PROTO_LIBRARIES="$HOME/aasdk/lib/libaasdk_proto.so" ../openauto

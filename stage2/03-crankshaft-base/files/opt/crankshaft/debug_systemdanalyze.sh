b59a327552d1f175e0a8f40a0691eeb9046edb26
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-07-28 17:06:32 +0200

    [script] Remove systemd analyzer command

--- stage2/03-crankshaft-base/files/opt/crankshaft/debug_systemdanalyze.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sleep 60
-/usr/local/bin/crankshaft filesystem boot unlock
-/usr/bin/systemd-analyze plot > /boot/crankshaft/startsequence.svg
-/usr/local/bin/crankshaft filesystem boot lock

36500a8ae19949bfaf7ff9603dd75e8aee862b66
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-08-01 19:24:35 +0200

    [script] Switch update service to md5

--- stage2/03-crankshaft-base/files/opt/crankshaft/service_update.sh
+++ stage2/03-crankshaft-base/files/opt/crankshaft/service_update.sh
@@ -4,25 +4,22 @@ source /opt/crankshaft/crankshaft_default_env.sh
 source /opt/crankshaft/crankshaft_system_env.sh
 source /boot/crankshaft/crankshaft_env.sh
 
-# Check udev rules
-REMOTE_UDEV=`wget --no-check-certificate https://raw.githubusercontent.com/opencardev/prebuilts/master/udev/51-android.rules --spider --server-response -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'`
-
-if [ -f /etc/udev/rules.d/51-android.master ]; then
-    LOCAL_UDEV=`wc -c /etc/udev/rules.d/51-android.master | awk '{print $1}'`
-else
-    LOCAL_UDEV=0
-fi
-
-# Check crankshaft amangement tool
-REMOTE_CSMT=`wget --no-check-certificate https://raw.githubusercontent.com/opencardev/prebuilts/master/csmt/crankshaft --spider --server-response -O - 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}'`
-
-if [ -f /usr/local/bin/crankshaft ]; then
-    LOCAL_CSMT=`wc -c /usr/local/bin/crankshaft | awk '{print $1}'`
-else
-    LOCAL_CSMT=0
-fi
 
 if [ "`ping -c 1 google.com`" ];then
+    # Check udev rules
+    wget -q -O /tmp/51-android.rules.md5 --no-check-certificate https://raw.githubusercontent.com/opencardev/prebuilts/master/udev/51-android.rules.md5
+    REMOTE_UDEV=$(cat /tmp/51-android.rules.md5 | awk {'print $1'})
+    LOCAL_UDEV=$(md5sum /etc/udev/rules.d/51-android.master | awk {'print $1'})
+    rm /tmp/51-android.rules.md5
+
+    # Check crankshaft amangement tool
+    wget -q --no-check-certificate -O /tmp/crankshaft.md5 https://raw.githubusercontent.com/opencardev/prebuilts/master/csmt/crankshaft.md5
+    REMOTE_CSMT=$(cat /tmp/crankshaft.md5 | awk {'print $1'})
+    LOCAL_CSMT=$(md5sum /usr/local/bin/crankshaft | awk {'print $1'})
+    rm /tmp/crankshaft.md5
+
+    # Notifications
+
     # udev
     if [ "$REMOTE_UDEV" != "$LOCAL_UDEV" ];then
         echo "[${RED}${BOLD} NOTE ${RESET}] *******************************************************" >/dev/tty3

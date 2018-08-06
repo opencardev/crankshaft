2711d101f344392a6df8e5bf191112ba1d9dd37a
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-08-07 01:50:44 +0200

    [script] Hotfix: ignition shutdown

--- stage2/03-crankshaft-base/files/opt/crankshaft/service_gpiotrigger.sh
+++ stage2/03-crankshaft-base/files/opt/crankshaft/service_gpiotrigger.sh
@@ -6,33 +6,31 @@ source /boot/crankshaft/crankshaft_env.sh
 IGN_COUNTER=0
 
 # check gpio pin if activated
-if [ $ENABLE_GPIO -eq 1 ]; then
-    if [ $REARCAM_PIN -ne 0 ] || [ $IGNITION_PIN -ne 0 ]; then
-        while true; do
-            if [ $REARCAM_PIN -ne 0 ]; then
-                REARCAM_GPIO=`gpio -g read $REARCAM_PIN`
-                if [ $REARCAM_GPIO -ne 1 ] ; then
-                    touch /tmp/rearcam_enabled
-                else
-                    if [ -f /tmp/rearcam_enabled ]; then
-                        rm /tmp/rearcam_enabled
-                    fi
+if [ $REARCAM_PIN -ne 0 ] || [ $IGNITION_PIN -ne 0 ]; then
+    while true; do
+        if [ $REARCAM_PIN -ne 0 ]; then
+            REARCAM_GPIO=`gpio -g read $REARCAM_PIN`
+            if [ $REARCAM_GPIO -ne 1 ] ; then
+                touch /tmp/rearcam_enabled
+            else
+                if [ -f /tmp/rearcam_enabled ]; then
+                    rm /tmp/rearcam_enabled
                 fi
             fi
-            if [ $IGNITION_PIN -ne 0 ]; then
-                IGNITION_GPIO=`gpio -g read $IGNITION_PIN`
-                if [ $IGNITION_GPIO -ne 0 ] ; then
-                    IGN_COUNTER=$((IGN_COUNTER+1))
-                    if [ $IGN_COUNTER -gt $IGNITION_DELAY ]; then
-                        touch /tmp/external_exit
-                    fi
-                else
-                    IGN_COUNTER=0
+        fi
+        if [ $IGNITION_PIN -ne 0 ]; then
+            IGNITION_GPIO=`gpio -g read $IGNITION_PIN`
+            if [ $IGNITION_GPIO -ne 0 ] ; then
+                IGN_COUNTER=$((IGN_COUNTER+1))
+                if [ $IGN_COUNTER -gt $IGNITION_DELAY ]; then
+                    touch /tmp/external_exit
                 fi
+            else
+                IGN_COUNTER=0
             fi
-            sleep 1
-        done
-    fi
+        fi
+        sleep 1
+    done
 fi
 
 exit 0

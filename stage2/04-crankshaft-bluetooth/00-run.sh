fdf2a44e2ce0e537b18e0fbb3709f6fa37e80e38
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-08-15 15:53:23 +0200

    [script] Fix missing copy files

--- stage2/04-crankshaft-bluetooth/00-run.sh
+++ stage2/04-crankshaft-bluetooth/00-run.sh
@@ -3,6 +3,7 @@
 # /etc
 install -m 644 files/etc/systemd/system/btautopair.service              "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/etc/systemd/system/btautoconnect.service           "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 644 files/etc/systemd/system/btdevicedetect.service          "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/etc/systemd/system/csng-bluetooth.service          "${ROOTFS_DIR}/etc/systemd/system/"
 install -m 644 files/etc/dbus-1/system.d/bluetooth2.conf                "${ROOTFS_DIR}/etc/dbus-1/system.d/"
 install -m 644 files/etc/dbus-1/system.d/dundee2.conf                   "${ROOTFS_DIR}/etc/dbus-1/system.d/"
@@ -13,6 +14,7 @@ install -d "${ROOTFS_DIR}/opt/crankshaft"
 install -m 755 files/opt/crankshaft/service_bluetooth.sh                "${ROOTFS_DIR}/opt/crankshaft/"
 install -m 755 files/opt/crankshaft/service_btautoconnect.sh            "${ROOTFS_DIR}/opt/crankshaft/"
 install -m 755 files/opt/crankshaft/service_btautopair.py               "${ROOTFS_DIR}/opt/crankshaft/"
+install -m 755 files/opt/crankshaft/service_btdevicedetect.sh           "${ROOTFS_DIR}/opt/crankshaft/"
 install -m 644 files/opt/crankshaft/start_order                         "${ROOTFS_DIR}/opt/crankshaft/"
 
 # /usr

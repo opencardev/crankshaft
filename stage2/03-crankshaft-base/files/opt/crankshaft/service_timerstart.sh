338c4ccb4ecca55f0fdd50ecb1857a3f4ac761aa
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-08-24 00:48:35 +0200

    [script] Optimize timer start

--- stage2/03-crankshaft-base/files/opt/crankshaft/service_timerstart.sh
+++ stage2/03-crankshaft-base/files/opt/crankshaft/service_timerstart.sh
@@ -9,9 +9,14 @@ fi
 if [ ! -f /tmp/dev_mode_enabled ]; then
     log_echo "Start timers"
     echo "[${CYAN}${BOLD} INFO ${RESET}] *******************************************************" >/dev/tty3
-    echo "[${CYAN}${BOLD} INFO ${RESET}] Starting Timers" >/dev/tty3
+    echo "[${CYAN}${BOLD} INFO ${RESET}] Starting timers" >/dev/tty3
     echo "[${CYAN}${BOLD} INFO ${RESET}] *******************************************************" >/dev/tty3
-    /usr/local/bin/crankshaft timers start
+    /usr/local/bin/crankshaft timers start &
+else
+    log_echo "Skip starting  timers"
+    echo "[${CYAN}${BOLD} INFO ${RESET}] *******************************************************" >/dev/tty3
+    echo "[${CYAN}${BOLD} INFO ${RESET}] Skipping start timers by dev mode enabled..." >/dev/tty3
+    echo "[${CYAN}${BOLD} INFO ${RESET}] *******************************************************" >/dev/tty3
 fi
 
 exit 0

7109648dbd0a525214b8ad4c0afa7117706381f0
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-08-29 20:33:57 +0200

    [script] Allow faster day/night switching

--- stage2/03-crankshaft-base/files/opt/crankshaft/service_daynight.sh
+++ stage2/03-crankshaft-base/files/opt/crankshaft/service_daynight.sh
@@ -27,14 +27,12 @@ fi
 if [ ! -z $1 ] && [ ! -z $2 ]; then
     if [ $1 == "app" ] && [ $2 == "day" ] && [ -f /tmp/night_mode_enabled ]; then
         log_echo "Save bnrightness night"
-        crankshaft brightness save
         sudo rm /tmp/night_mode_enabled
         log_echo "Restore bnrightness day"
         crankshaft brightness restore
     fi
     if [ $1 == "app" ] && [ $2 == "night" ] && [ ! -f /tmp/night_mode_enabled ]; then
         log_echo "Save bnrightness day"
-        crankshaft brightness save
         touch /tmp/night_mode_enabled
         chmod 666 /tmp/night_mode_enabled
         log_echo "Restore bnrightness night"

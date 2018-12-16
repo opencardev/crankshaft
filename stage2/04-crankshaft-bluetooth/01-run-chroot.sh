00ead69376f1a12c9477a1f4175ded01cb667e27
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-12-17 00:41:07 +0100

    [script] Add bt packages / Adjust dnsmasq

--- stage2/04-crankshaft-bluetooth/01-run-chroot.sh
+++ stage2/04-crankshaft-bluetooth/01-run-chroot.sh
@@ -21,3 +21,19 @@ echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt
 usermod -G bluetooth -a pi
 usermod -G bluetooth -a pulse
 usermod -G bluetooth -a root
+
+# Enable compat and disable sap
+sed -i 's/ExecStart=.*/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd --compat --noplugin=sap/' /lib/systemd/system/bluetooth.service
+
+#link test script files
+ln -s /usr/share/doc/bluez-test-scripts/examples/list-devices /usr/local/bin/list-devices
+ln -s /usr/share/doc/bluez-test-scripts/examples/monitor-bluetooth /usr/local/bin/monitor-bluetooth
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-adapter /usr/local/bin/test-adapter
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-heartrate /usr/local/bin/test-heartrate
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-manager /usr/local/bin/test-manager
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-nap /usr/local/bin/test-nap
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-network /usr/local/bin/test-network
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-profile /usr/local/bin/test-profile
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-proximity /usr/local/bin/test-proximity
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-sap-server /usr/local/bin/test-sap-server
+ln -s /usr/share/doc/bluez-test-scripts/examples/test-thermometer /usr/local/bin/test-thermometer

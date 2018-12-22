c77fea49d91aee18931da8446d34ee4739c10fe1
Marc Hillesheim <hawkeyexp@googlemail.com>
2018-12-22 17:28:55 +0100

    [script] Fix: Add wallpaper-eq to backup script

--- stage2/03-crankshaft-base/files/etc/initramfs-tools/scripts/local-top/local.sh
+++ stage2/03-crankshaft-base/files/etc/initramfs-tools/scripts/local-top/local.sh
@@ -76,6 +76,7 @@ for FSMOUNTPOINT in $(ls -d /media/USBDRIVES/* | grep -v CSSTORAGE); do
         cp -f /tmp/bootfs/crankshaft/wallpaper-night.png /media/USBDRIVES/${PARTITION}/cs-backup/${SERIAL}/boot/crankshaft/ 2>/dev/null
         cp -f /tmp/bootfs/crankshaft/wallpaper-classic.png /media/USBDRIVES/${PARTITION}/cs-backup/${SERIAL}/boot/crankshaft/ 2>/dev/null
         cp -f /tmp/bootfs/crankshaft/wallpaper-classic-night.png /media/USBDRIVES/${PARTITION}/cs-backup/${SERIAL}/boot/crankshaft/ 2>/dev/null
+        cp -f /tmp/bootfs/crankshaft/wallpaper-eq.png /media/USBDRIVES/${PARTITION}/cs-backup/${SERIAL}/boot/crankshaft/ 2>/dev/null
         cp -f /tmp/bootfs/crankshaft/splash.png /media/USBDRIVES/${PARTITION}/cs-backup/${SERIAL}/boot/crankshaft/ 2>/dev/null
         cp -f /tmp/bootfs/crankshaft/shutdown.png /media/USBDRIVES/${PARTITION}/cs-backup/${SERIAL}/boot/crankshaft/ 2>/dev/null
         cp -f /tmp/bootfs/crankshaft/button_1 /media/USBDRIVES/${PARTITION}/cs-backup/${SERIAL}/boot/crankshaft/ 2>/dev/null

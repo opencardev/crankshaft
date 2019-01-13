99a73ac3c76e2f78e526ab8b85f868406838973f
Marc Hillesheim <hawkeyexp@googlemail.com>
2019-01-13 20:37:05 +0100

    [script] Adjust bt pairing

--- stage3/04-crankshaft-bluetooth/files/opt/crankshaft/service_bluetooth.sh
+++ stage3/04-crankshaft-bluetooth/files/opt/crankshaft/service_bluetooth.sh
@@ -16,24 +16,28 @@ EOF
 }
 
 if [ $ENABLE_BLUETOOTH -eq 1 ]; then
-    touch /tmp/button_bluetooth_visible
+    touch /tmp/bluetooth_enabled
 
     # Set controller in correct mode
     log_echo "Init bluetooth adapter defaults"
     sudo bt-adapter --set Powered 1
     sudo bt-adapter --set DiscoverableTimeout 0
     sudo bt-adapter --set Discoverable 1
-    sudo bt-adapter --set PairableTimeout 120
-    if [ $ENABLE_PAIRABLE -eq 1 ]; then
-        sudo bt-adapter --set Pairable 1
-        log_echo "Set bluetooth adapter pairable for 120 seconds"
-    fi
     # Try 5 attempts to connect
     counter=0
     while [ $counter -lt 5 ] && [ ! -f /tmp/btdevice ]; do
-        sleep 5
-        cs_autoconnect > /dev/null 2>&1
         log_echo "Bluetooth auto connect attempt: $counter"
+        cs_autoconnect > /dev/null 2>&1
+        sleep 5
         counter=$((counter+1))
     done
+
+    if [ $ENABLE_PAIRABLE -eq 1 ]; then
+        log_echo "Set bluetooth adapter pairable for 120 seconds"
+        sudo bt-adapter --set Pairable 1
+        sudo bt-adapter --set PairableTimeout 120
+        touch /tmp/bluetooth_pairable
+        sleep 120
+        sudo rm /tmp/bluetooth_pairable
+    fi
 fi

2253628df43cc11bf0ac1e8f1900bc411806e236
Marc Hillesheim <hawkeyexp@googlemail.com>
2019-01-15 11:00:45 +0100

    [script] Unblock bt script

--- stage3/04-crankshaft-bluetooth/files/opt/crankshaft/service_bluetooth.sh
+++ stage3/04-crankshaft-bluetooth/files/opt/crankshaft/service_bluetooth.sh
@@ -21,8 +21,15 @@ if [ $ENABLE_BLUETOOTH -eq 1 ]; then
     # Set controller in correct mode
     log_echo "Init bluetooth adapter defaults"
     sudo bt-adapter --set Powered 1
-    sudo bt-adapter --set DiscoverableTimeout 0
     sudo bt-adapter --set Discoverable 1
+    sudo bt-adapter --set DiscoverableTimeout 0
+    if [ $ENABLE_PAIRABLE -eq 1 ]; then
+        log_echo "Set bluetooth adapter pairable for 120 seconds"
+        sudo bt-adapter --set Pairable 1
+        sudo bt-adapter --set PairableTimeout 120
+        touch /tmp/bluetooth_pairable
+        sleep 120 && sudo rm /tmp/bluetooth_pairable &
+    fi
     # Try 5 attempts to connect
     counter=0
     while [ $counter -lt 5 ] && [ ! -f /tmp/btdevice ]; do
@@ -31,13 +38,4 @@ if [ $ENABLE_BLUETOOTH -eq 1 ]; then
         sleep 5
         counter=$((counter+1))
     done
-
-    if [ $ENABLE_PAIRABLE -eq 1 ]; then
-        log_echo "Set bluetooth adapter pairable for 120 seconds"
-        sudo bt-adapter --set Pairable 1
-        sudo bt-adapter --set PairableTimeout 120
-        touch /tmp/bluetooth_pairable
-        sleep 120
-        sudo rm /tmp/bluetooth_pairable
-    fi
 fi

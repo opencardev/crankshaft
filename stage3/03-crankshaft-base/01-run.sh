1c698aa0756b26a89c120f3e14f926813dba29ff
Marc Hillesheim <hawkeyexp@googlemail.com>
2019-01-25 13:35:07 +0100

    [script] Fix file mode

--- stage3/03-crankshaft-base/01-run.sh
+++ stage3/03-crankshaft-base/01-run.sh
@@ -116,7 +116,7 @@ install -m 644 files/opt/crankshaft/wallpaper/camera-overlay.png              "$
 install -m 644 files/opt/crankshaft/wallpaper/wallpaper-eq.png                "${ROOTFS_DIR}/opt/crankshaft/wallpaper/"
 
 install -d "${ROOTFS_DIR}/opt/crankshaft/cam_overlay"
-install -m 644 files/opt/crankshaft/cam_overlay/cam_overlay.bin   "${ROOTFS_DIR}/opt/crankshaft/cam_overlay/"
+install -m 755 files/opt/crankshaft/cam_overlay/cam_overlay.bin   "${ROOTFS_DIR}/opt/crankshaft/cam_overlay/"
 install -m 644 files/opt/crankshaft/cam_overlay/overlay.png       "${ROOTFS_DIR}/opt/crankshaft/cam_overlay/"
 install -m 644 files/opt/crankshaft/cam_overlay/shader-YUYV.frag  "${ROOTFS_DIR}/opt/crankshaft/cam_overlay/"
 install -m 644 files/opt/crankshaft/cam_overlay/shader.frag       "${ROOTFS_DIR}/opt/crankshaft/cam_overlay/"

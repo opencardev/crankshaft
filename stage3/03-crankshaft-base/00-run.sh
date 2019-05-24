b2809dce8b4b98f74eefffc8af2246918773e9a3
Marc Hillesheim <hawkeyexp@googlemail.com>
2019-05-24 23:32:10 +0200

    [script] Fix filename

--- stage3/03-crankshaft-base/00-run.sh
+++ stage3/03-crankshaft-base/00-run.sh
@@ -79,5 +79,5 @@ chmod 644 files/usr/local/bin/usbreset.md5
 
 # qt5
 # combine splitted archive
-cat $BASE_DIR/prebuilts/qt5/Qt_5123_OpenGLES2.tar.xz.part* > $BASE_DIR/prebuilts/qt5/Qt_5113_OpenGLES2.tar.xz
+cat $BASE_DIR/prebuilts/qt5/Qt_5123_OpenGLES2.tar.xz.part* > $BASE_DIR/prebuilts/qt5/Qt_5123_OpenGLES2.tar.xz
 mv -f $BASE_DIR/prebuilts/qt5/Qt_5123_OpenGLES2.tar.xz files/qt5/Qt5_OpenGLES2.tar.xz

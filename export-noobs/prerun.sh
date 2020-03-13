cf946ce668c58e9447f862861947beb96dbd3178
root <root@debian.f35.pl>
2020-03-13 21:00:23 +0100

    Fix lotetup resource temporarily unavailable error added function to terun losetup

--- export-noobs/prerun.sh
+++ export-noobs/prerun.sh
@@ -16,8 +16,10 @@ BOOT_LENGTH=$(echo "$PARTED_OUT" | grep -e '^1:' | cut -d':' -f 4 | tr -d B)
 ROOT_OFFSET=$(echo "$PARTED_OUT" | grep -e '^2:' | cut -d':' -f 2 | tr -d B)
 ROOT_LENGTH=$(echo "$PARTED_OUT" | grep -e '^2:' | cut -d':' -f 4 | tr -d B)
 
-BOOT_DEV=$(losetup --show -f -o "${BOOT_OFFSET}" --sizelimit "${BOOT_LENGTH}" "${IMG_FILE}")
-ROOT_DEV=$(losetup --show -f -o "${ROOT_OFFSET}" --sizelimit "${ROOT_LENGTH}" "${IMG_FILE}")
+BOOT_DEV=$(run_flappy 'losetup --show -v -f -o "${BOOT_OFFSET}" --sizelimit "${BOOT_LENGTH}" /dev/loop6 "${IMG_FILE}"')
+
+ROOT_DEV=$(run_flappy 'losetup --show -v -f -o "${ROOT_OFFSET}" --sizelimit "${ROOT_LENGTH}" /dev/loop4 "${IMG_FILE}"')
+
 echo "/boot: offset $BOOT_OFFSET, length $BOOT_LENGTH"
 echo "/:     offset $ROOT_OFFSET, length $ROOT_LENGTH"
 

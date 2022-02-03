ae7483704a231645be1a1124a759b07a0ab97767
Matt Hilton <matthilton2005@gmail.com>
2022-02-03 21:34:09 +0000

    Added workflow to add pi2 image

---
+++ stage4/01-armv7-fixes/00-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+# qt5 from prebuilts
+rm -rf files/qt5/Qt5_OpenGLES2.tar.xz
+cat $BASE_DIR/prebuilts/qt5/Qt_5151_armv7l_OpenGLES2.tar.xz* > files/qt5/Qt5_OpenGLES2.tar.xz
+
+#qt5
+tar -xf files/qt5/Qt5_OpenGLES2.tar.xz -C ${ROOTFS_DIR}/
